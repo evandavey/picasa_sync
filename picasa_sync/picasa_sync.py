e382897f67ca19a02b4c8fa8d7f23b9b765dca63
Evan Davey <evan.davey@cochranedavey.com>
2012-11-29 13:57:15 +0000

    bug fixes

--- picasa_sync/picasa_sync.py
+++ picasa_sync/picasa_sync.py
@@ -16,7 +16,10 @@ import subprocess
 import csv
 import StringIO
 import logging
-import os
+import os, sys
+import argparse
+from argparse import RawTextHelpFormatter
+
 logger = logging.getLogger('picasa-backup')
 
 logger.setLevel(logging.INFO)
@@ -34,72 +37,89 @@ photos_path = os.path.join('/media/data/photos')
 
 
 def get_albums():
-	cmd = ['google', 'picasa','list-albums']
-	cmds = " ".join(cmd)
-	logger.debug('Running: %(cmds)s' % locals())
-	p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	
+    cmd = ['google', 'picasa','list-albums']
+    cmds = " ".join(cmd)
+    logger.debug('Running: %(cmds)s' % locals())
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
-	out,err = p.communicate()
 
-	f = StringIO.StringIO(out)
-	reader =csv.reader(f)
+    out,err = p.communicate()
 
-	albums={}
-	for row in reader:
-		albums[row[0]]={'name':row[0],'url':row[1]}
+    f = StringIO.StringIO(out)
+    reader =csv.reader(f)
 
-	return albums
+    albums={}
+    for row in reader:
+        albums[row[0]]={'name':row[0],'url':row[1]}
+
+    return albums
 
 def create_album(album):
 
-	cmd = ['google', 'picasa','create','%(album)s' % locals()]
-	cmds = " ".join(cmd)
-	logger.debug('Running: %(cmds)s' % locals())
-	p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    cmd = ['google', 'picasa','create','%(album)s' % locals()]
+    cmds = " ".join(cmd)
+    logger.debug('Running: %(cmds)s' % locals())
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
-	out,err=p.communicate()
+    out,err=p.communicate()
 
-	if len(err)>0:
-		return True
-	else:
-		print err
-		return False
+    if len(err)>0:
+        return True
+    else:
+        print err
+        return False
 
 def upload_photo(album,photo):
 
 
-	cmd = ['google', 'picasa','post','--title=%(album)s' % locals(),'--src=%(photo)s' % locals()]
-	cmds = " ".join(cmd)
-	logger.debug('Running: %(cmds)s' % locals())
-	p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    cmd = ['google', 'picasa','post','--title=%(album)s' % locals(),'--src=%(photo)s' % locals()]
+    cmds = " ".join(cmd)
+    logger.debug('Running: %(cmds)s' % locals())
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
-	out,err=p.communicate()
+    out,err=p.communicate()
 
-	if len(err)>0:
-		return True
-	else:
-		print err
-		return False
+    if len(err)>0:
+        return True
+    else:
+        print err
+        return False
+
+
+def download_photo(album,photo):
+
+
+    cmd = ['google', 'picasa','get','--title=%(album)s' % locals(),'--src=%(photo)s' % locals()]
+    cmds = " ".join(cmd)
+    logger.debug('Running: %(cmds)s' % locals())
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+    out,err=p.communicate()
+
+    if len(err)>0:
+        return True
+    else:
+        print err
+        return False
 
 def get_photos(album):
 
-	cmd = ['google', 'picasa','list','--title=%(album)s' % locals()]
-	cmds = " ".join(cmd)
-	logger.debug('Running: %(cmds)s' % locals())
-	p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    cmd = ['google', 'picasa','list','--title=%(album)s' % locals()]
+    cmds = " ".join(cmd)
+    logger.debug('Running: %(cmds)s' % locals())
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
-	out,err = p.communicate()
+    out,err = p.communicate()
 
-	f = StringIO.StringIO(out)
-	reader =csv.reader(f)
+    f = StringIO.StringIO(out)
+    reader =csv.reader(f)
 
-	photos={}
-	for row in reader:
-		fileName, fileExtension = os.path.splitext(row[0])
-		photos[fileName]={'name':fileName,'url':row[1],'extension':fileExtension}
+    photos={}
+    for row in reader:
+        fileName, fileExtension = os.path.splitext(row[0])
+        photos[fileName]={'name':fileName,'url':row[1],'extension':fileExtension}
 
-	return photos
+    return photos
 
 
 def main(argv=None):
@@ -107,7 +127,7 @@ def main(argv=None):
     global r
 
     if argv is None:
-           argv = sys.argv
+        argv = sys.argv
 
 
     parser = argparse.ArgumentParser(
@@ -116,10 +136,10 @@ def main(argv=None):
     parser.add_argument('photos_path',help="path to local photos")
 
     try:
-       args = parser.parse_args()
+        args = parser.parse_args()
     except:
-       sys.exit(1)
-       
+        sys.exit(1)
+
     photos_path = args.photos_path
 
     albums={}
@@ -130,44 +150,47 @@ def main(argv=None):
 
     for dirname, dirnames, filenames in os.walk(photos_path):
         for subdirname in dirnames:
-        	if subdirname in albums:
-        		logger.info('%(subdirname)s album exists!' % locals())
-        		photos=get_photos(subdirname)
-        		num_photos=len(photos)
-        		logger.info('%(subdirname)s: %(num_photos)d existing photos' % locals())
-        	else:
-        		photos={}
-        		num_photos=len(photos)
-        		logger.debug('%(subdirname)s album does not exist' % locals())
-        		if create_album(subdirname):
-        			logger.info('Created album %(subdirname)s' % locals())
-        		else:
-        			logger.warning('Could not create %(subdirname)s' % locals())
-        			continue
+            if subdirname in albums:
+                logger.info('%(subdirname)s album exists!' % locals())
+                photos=get_photos(subdirname)
+                num_photos=len(photos)
+                logger.info('%(subdirname)s: %(num_photos)d existing photos' % locals())
+            else:
+                photos={}
+                num_photos=len(photos)
+                logger.debug('%(subdirname)s album does not exist' % locals())
+                if create_album(subdirname):
+                    logger.info('Created album %(subdirname)s' % locals())
+                else:
+                    logger.warning('Could not create %(subdirname)s' % locals())
+                    continue
 
-    	for dirname2, dirnames2, filenames in os.walk(os.path.join(dirname,subdirname)):
-     	   	num_files=len(filenames)
+        for dirname2, dirnames2, filenames in os.walk(os.path.join(dirname,subdirname)):
+            num_files=len(filenames)
 
-    		if num_files != num_photos:
-    			logger.warning('%(subdirname)s: local Files %(num_files)d, Web Photos: %(num_photos)s' % locals())
-    		for f in filenames:
-    			filename, fileExtension = os.path.splitext(f)
-    			if filename in photos:
-    			    photos[filename]['synced']=True
-    				logger.debug('%(subdirname)s: %(filename)s exists, skipping' % locals())
-    			else:
-    				logger.debug('%(subdirname)s: %(filename)s does not exist' % locals())
-    				if upload_photo(subdirname,os.path.join(dirname,subdirname,f)):
-    					logger.info('%(subdirname)s: Uploaded %(filename)s' % locals())
-    					photos[filename]['synced']=True
-    				else:
-    				    
-    				    photos[filename]['synced']=False
-    					logger.warning('%(subdirname)s: Failed to upload %(filename)s' % locals())
-    					
-    		for k,p in photos.iteritems():
-    		    if not p['synced'] and p['url']:
-    		        logger.info('%(subdirname)s: Downloading %(filename)s' % locals())
+            if num_files != num_photos:
+                logger.warning('%(subdirname)s: local Files %(num_files)d, Web Photos: %(num_photos)s' % locals())
+            for f in filenames:
+                filename, fileExtension = os.path.splitext(f)
+                if filename in photos:
+                    photos[filename]['synced']=True
+                    logger.debug('%(subdirname)s: %(filename)s exists, skipping' % locals())
+                else:
+                    logger.debug('%(subdirname)s: %(filename)s does not exist' % locals())
+                    photos[filename] = {}
+                    if upload_photo(subdirname,os.path.join(dirname,subdirname,f)):
+                        logger.info('%(subdirname)s: Uploaded %(filename)s' % locals())
+                        
+                        photos[filename]['synced']=True
+                    else:
+
+                        photos[filename]['synced']=False
+                        logger.warning('%(subdirname)s: Failed to upload %(filename)s' % locals())
+
+            for k,p in photos.iteritems():
+                if not p['synced'] and p['url']:
+                    print p['url']
+                    logger.info('%(subdirname)s: Downloading %(filename)s' % locals())
 
 if __name__ == "__main__":
 
